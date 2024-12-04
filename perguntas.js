34b35d5d445b0d212c9fe426b00443f75f432238
2c207970 <wkprodc@gmail.com>
2024-12-04 08:13:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Naruto',
+    'Quantos tomoe Sasuke tem?',
+    'seis tomoes'
+)
+
+criaCartao(
+    'Naruto',
+    'Quem é a mãe da Sarada?',
+    'Sakura'
+)
+
+criaCartao(
+    'Demon Slyer',
+    'Qual é a respiração do Inosuke?',
+    'Respiração da besta'
+)
+
+criaCartao(
+    'Ataque dos Titãs',
+    'Qual éra o titãn de Eren Kruguer?',
+    'Titãn de Ataque'
+)
